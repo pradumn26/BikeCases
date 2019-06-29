fe25e57ed927e92332a53ab43f3f96f36dc822ec
pradumn26 <pradumn.26@gmail.com>
2019-06-29 12:27:27 +0530

    passport setup

--- index.js
+++ index.js
@@ -8,6 +8,17 @@ mongoUtil.connectToDb()
 .then(function () {
     const app = express();
 
+    app.use(express.urlencoded({extended: true}));
+    app.use(express.json());
+    app.use(cookieSession({
+        maxAge: 30*24*3600*1000,
+        keys: ['djdnkjkdn']
+    }));
+    app.use(passport.initialize());
+    app.use(passport.session());
+
+    require('./util/passportUtil');
+
     const PORT = process.env.PORT || 4000;
     app.listen(PORT, function () {
         console.log(`App has started on port ${PORT}`);
---
+++ util/passportUtil.js
@@ -0,0 +1,63 @@
+const passport = require('passport');
+const LocalStrategy = require('passport-local').Strategy;
+const ObjectId = require('mongodb').ObjectId;
+
+const db = require('./mongoUtil').getDb();
+
+passport.serializeUser(function(document, done) {
+    done(null, {id:document._id, authType: document.authType});
+});
+
+passport.deserializeUser(function(data, done) {
+    let col = data.authType == 'police-auth' ? 'police' : 'users';
+    let collection = db.collection(col);
+
+    collection.findOne({_id: ObjectId(data.id)})
+        .then(function (doc) {
+            doc.authType = data.authType;
+            done(null, doc);
+        })
+        .catch(function (err) {
+            done(err);
+        })
+});
+
+passport.use('police-auth', new LocalStrategy(
+    {usernameField: 'email', passwordField: 'password'},
+    function(email, password, done) {
+        let doctorsCollection = db.collection('police');
+
+        console.log(email, password);
+
+        doctorsCollection.findOne({email, password})
+            .then(function (doctor) {
+                if (!doctor)
+                    return done(null, false);
+
+                doctor.authType = 'police-auth';
+                return done(null, doctor);
+            })
+            .catch(function (err) {
+                done(err);
+            })
+    }
+));
+
+passport.use('users-auth', new LocalStrategy(
+    {usernameField: 'email', passwordField: 'password'},
+    function(email, password, done) {
+        let usersCollection = db.collection('users');
+
+        usersCollection.findOne({email, password})
+            .then(function (user) {
+                if (!user)
+                    return done(null, false);
+
+                user.authType = 'users-auth';
+                return done(null, user);
+            })
+            .catch(function (err) {
+                done(err);
+            })
+    }
+));
